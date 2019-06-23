b557d6db0177cbf9f84537cc30009d85630778b7
cassio entrudo <cassio.entrudo@gmail.com>
2019-06-23 12:50:03 -0300

    alterações by Rodolfo

--- source/main.py
+++ source/main.py
@@ -18,6 +18,13 @@ from neural_network import Neural_Network
 
 numFolds=10
 alpha=0.2
+numAlpha = 3
+numFR = 3
+numCamadas = 3
+numParada = 3
+conParada = 0.00015
+tamCamadas = 3
+initFR=0.100
 
 
 def categoricVotation(network, testFold, targetFeature, testDataset):
@@ -40,8 +47,8 @@ def categoricVotation(network, testFold, targetFeature, testDataset):
                 minDif=abs(fx-possibleAnswers[j])
                 minAnswer=possibleAnswers[j]
         answers.extend([minAnswer])
-    print("Resultado: ", answers)
-    print("Corretos: ", correct)
+    #print("Resultado: ", answers)
+    #print("Corretos: ", correct)
     return answers,correct
 
 def initialize_network_for_validation(network_file_lines, initial_weights_file_lines, dataset_file_lines, isTest, network = None):
@@ -115,25 +122,25 @@ def initialize_network_for_validation(network_file_lines, initial_weights_file_l
     return errorReg, network, fx
 
 
-def save_results(neural_network_structure, total_rights, total_wrongs):
-    full_string = "Quantidade de Folds: "
-    full_string += str(numFolds)
-    full_string += '\n\n'
+def save_results(neural_network_structure, vp, vn, fp, fn, usedAlpha, conParada):
+    full_string = "Condição de parada difJ < "
+    full_string += str(conParada)
+    full_string += '\n'
     full_string += "Alpha: "
-    full_string += str(alpha)
-    full_string += '\n\n'
+    full_string += str(usedAlpha)
+    full_string += '\n'
     full_string += "Fator de Regularização : "
     full_string += str(neural_network_structure[0])
-    full_string += '\n\n'
+    full_string += '\n'
     full_string += "Quantidade de entradas : "
     full_string += str(neural_network_structure[1])
-    full_string += '\n\n'
+    full_string += '\n'
     full_string += "Quantidade de saídas: "
     full_string += str(neural_network_structure[-1])
-    full_string += '\n\n'
+    full_string += '\n'
     full_string += "Quantidade de camadas ocultas: "
     full_string += str(len(neural_network_structure) - 3)
-    full_string += '\n\n'
+    full_string += '\n'
     
     
     for i in range(2, len(neural_network_structure)-1, 1):
@@ -141,16 +148,76 @@ def save_results(neural_network_structure, total_rights, total_wrongs):
         full_string += str(neural_network_structure[i])
         full_string += '\n'
     
+    full_string += "VP: "
+    full_string += str(vp)
     full_string += '\n'
-    full_string += "Acertos: "
-    full_string += str(total_rights)
+    full_string += "VN: "
+    full_string += str(vn)
     full_string += '\n'
-    full_string += "Erros: "
-    full_string += str(total_wrongs)
+    full_string += "FP: "
+    full_string += str(fp)
+    full_string += '\n'
+    full_string += "FN: "
+    full_string += str(fn)
+    full_string += '\n\n'
     
-    f= open("Results.txt","w+")
-    f.write(full_string)
+    
+    
+    with open("Results.txt","a+") as f:
+        f.write(full_string)
 
+
+def execute_once(neural_network_structure, alpha, folds, condParada):
+    vp = 0
+    vn = 0
+    fp = 0
+    fn = 0
+    for i in range(len(folds)):
+        empty_initial_weights = []
+        testFold=folds[i]
+        numVector = list(range(len(testFold)))
+        testFold.index=numVector
+        datasets=pd.DataFrame();
+        for j in range(len(folds)):
+            if (j!=i):
+                datasets = datasets.append(folds[j])
+        dataset = datasets.values.tolist()
+        testset = testFold.values.tolist()
+        fixed_dataset = []
+        test_dataset = []
+
+        #print("dataset", dataset)
+        for k in range(len(dataset)):
+            fixed_dataset.append(str(dataset[k])[1:-1])
+        
+        for k in range(len(testFold)):
+            test_dataset.append(str(testset[k])[1:-1])
+            
+        
+        print("Executing with fold number: ", i, " and neural_network_structure=", neural_network_structure)
+        errorReg,network,fx = initialize_network_for_validation(neural_network_structure, empty_initial_weights, fixed_dataset, False)
+        difError=errorReg
+        while (abs(difError)>condParada):
+            difError=errorReg
+            network.a=[]
+            network.z=[]
+            errorReg,network,fx = initialize_network_for_validation(neural_network_structure, empty_initial_weights, fixed_dataset, False, network)
+            difError-=errorReg
+            #print("Fx: ", fx)
+            print("difError: ", difError)
+        answers,correct = categoricVotation(network,testFold,table.columns.size -1, test_dataset)
+        for j in range(len(answers)):
+            if(answers[j]==correct[j]):
+                if(answers[j]==0):
+                    vn += 1
+                else:
+                    vp += 1
+            else:
+                if(answers[j]==0):
+                    fn += 1
+                else:
+                    fp += 1
+    return vp, vn, fp, fn
 
 def main():
     folds = k_folds.k_folding(table, numFolds, table.columns[table.columns.size-1])
@@ -173,58 +240,23 @@ def main():
     
         initialize_network_for_validation(network_file_lines, initial_weights_file_lines, dataset_file_lines, True)
     else:
-        empty_initial_weights = []
     
         #print("empty_initial_weights=", empty_initial_weights)
-        
-        total_rights = 0
-        total_wrongs = 0
-        
-        for i in range(len(folds)):
-            testFold=folds[i]
-            numVector = list(range(len(testFold)))
-            testFold.index=numVector
-            datasets=pd.DataFrame();
-            for j in range(len(folds)):
-                if (j!=i):
-                    datasets = datasets.append(folds[j])
-            dataset = datasets.values.tolist()
-            testset = testFold.values.tolist()
-            fixed_dataset = []
-            test_dataset = []
-    
-            #print("dataset", dataset)
-            for k in range(len(dataset)):
-                fixed_dataset.append(str(dataset[k])[1:-1])
-            
-            for k in range(len(testFold)):
-                test_dataset.append(str(testset[k])[1:-1])
-                
-            neural_network_structure = [0.250, table.columns.size -1, 40, 40, 1 ]
-            print("Executing with fold number: ", i, " and neural_network_structure=", neural_network_structure)
-            errorReg,network,fx = initialize_network_for_validation(neural_network_structure, empty_initial_weights, fixed_dataset, False)
-            difError=errorReg
-            while (abs(difError)>0.0001):
-                difError=errorReg
-                network.a=[]
-                network.z=[]
-                errorReg,network,fx = initialize_network_for_validation(neural_network_structure, empty_initial_weights, fixed_dataset, False, network)
-                difError-=errorReg
-                #print("Fx: ", fx)
-                print("difError: ", difError)
-            answers,correct = categoricVotation(network,testFold,table.columns.size -1, test_dataset)
-            right = 0
-            wrong = 0
-            for j in range(len(answers)):
-                if(answers[j]==correct[j]):
-                    right += 1
-                else:
-                    wrong += 1
-            print("Acertos: ", right, "Erros: ", wrong )
-            total_rights += right
-            total_wrongs += wrong
-            
-        save_results(neural_network_structure, total_rights, total_wrongs)
+        for k in range(1,numParada+1):
+            for i in range(1,numAlpha+1):
+                for j in range(1,numFR+1):
+                    for l in range(1,tamCamadas+1):
+                        for m in range (1,numCamadas+1):
+                        
+                            #neural_network_structure = [0.250, table.columns.size -1, 40, 40, 1 ]
+                            neural_network_structure = []
+                            neural_network_structure.extend([initFR*j])
+                            neural_network_structure.extend([table.columns.size -1])
+                            for n in range (1,m+1):
+                                neural_network_structure.extend([l*table.columns.size -1])
+                            neural_network_structure.extend([1])
+                            vp, vn, fp, fn = execute_once(neural_network_structure, i*alpha, folds, conParada/k)
+                            save_results(neural_network_structure, vp, vn, fp, fn, i*alpha, conParada/k)
             
             
 
